40dd0188a1728c0d2596454c57e72a783f68f844
Tim Ruffles <oi@truffles.me.uk>
2015-01-25 14:59:26 +0000

    make worse

--- testr.js
+++ testr.js
@@ -6,3 +6,11 @@ function hello(x) {
     console.log("yup");
   }
 }
+
+function hello(x) {
+  if(x == 0) {
+    console.log("jshint will like this") && console.log("yup");
+    return
+    {};
+  }
+}
