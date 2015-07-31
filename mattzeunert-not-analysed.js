b523a8792c88715ea64ab380e68e41c5e01f7842
Tim Ruffles <oi@truffles.me.uk>
2015-07-31 11:01:00 +0100

    check

--- mattzeunert-not-analysed.js
+++ mattzeunert-not-analysed.js
@@ -1,1 +1,13 @@
-function hi() {}
+  function ddd() {
+        console && console.log("cake");
+              a = "global";
+                }
+
+ddd()
+
+  sth = "global var"
+
+
+  adsfsdf
+      dsa
+      fs
