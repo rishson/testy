8731d255ae8c9bfca861cd52cf08a222fc31256d
Tim Ruffles <oi@truffles.me.uk>
2015-08-14 11:51:55 +0100

    another

--- append.js
+++ append.js
@@ -193,3 +193,4 @@ foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
+foo(); function p(x) { x + y && z() }
