4a0cb673b8837aa5bdfcaa8bdbb7cf74a3051bf2
Tim Ruffles <oi@truffles.me.uk>
2015-10-21 15:24:11 +0100

    another

--- added-1444996337.js
+++ added-1444996337.js
@@ -1,5 +1,12 @@
 foo();
 
-function p(x) { x + y && z() }
-foo(); function p(x) { x + y && z() }
+function p(x) {
+  x + y && z() 
+}
+foo();
+
+function p(x) { 
+  x + y && z() 
+}
+
 foo(); function p(x) { x + y && z() }
