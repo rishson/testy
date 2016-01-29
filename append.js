438776e68a4b4eaf2a4fd55d3c2f00bf128d0ff2
Tim Ruffles <timruffles@gmail.com>
2016-01-29 10:51:51 +0000

    another

--- append.js
+++ append.js
@@ -214,3 +214,4 @@ foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
+foo(); function p(x) { x + y && z() }
--- badClass.coffee
+++ badClass.coffee
@@ -1,2 +1,5 @@
 class boaConstrictor
   bumb: "oh noes"
+  
+  
+
