e844f4929d5ea3e774d24acba072e51a1db53141
Andy Richardson <rishson@gmail.com>
2016-02-02 11:16:54 +0000

    another

--- append.js
+++ append.js
@@ -236,3 +236,4 @@ foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
 foo(); function p(x) { x + y && z() }
+foo(); function p(x) { x + y && z() }
